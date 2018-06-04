d9e2fb3560249d8cff441b90716ba89c90a5e9dc
Kevin David Heritage <kheritage222@gmail.com>
2018-06-04 21:20:34 +0200

    Creates a router that goes home
    package.json - adds a package for the history
    src/app/App.jsx - adds the default router
    src/index.js - removes previous router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import TitleBar from './TitleBar.jsx';
 import NavDrawer from './NavDrawer/NavDrawer.jsx';
+import { Router, Route, withRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 const loginButton = (<Button color="inherit">Login</Button>);
 const title = 'Media Org';
 
+if (window.location.pathname === "/index.html") window.location.href = "/";
+const Home = () => (<div>Home</div>);
+
 class App extends React.Component {
+
     render() {
-        return (<div>
-            {/* <TitleBar title={title} navButtonAction={toggleDrawer}>
-                {loginButton}
-                </TitleBar> */}
-            <NavDrawer />
-        </div>);
-    };
+        return (<Router history={createBrowserHistory()}>
+            <div>
+                <Route exact path="/" component={Home} />
+            </div>
+        </Router>);
+    }
 };
 
 export default App;
